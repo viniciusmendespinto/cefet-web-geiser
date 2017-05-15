4b3505350ec7c1b9cc334b5c25df3782a57d90dc
ViniciusMP <viniciusloco18@hotmail.com>
2017-05-15 17:07:44 -0300

    Pratica do Geiser

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-var express = require('express'),
+let express = require('express'),
     app = express();
 
 app.use(express.static('client/'));
@@ -10,21 +10,21 @@ app.listen(3000, function () {
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
-var db = {
+
+let fs = require('fs');
+_ = require('underscore'),
+let db = {
+	jogadores: JSON.parse(fs.readFileSync(__dirname + '/data/jogadores.json')),
+    jogosPorJogador: JSON.parse(fs.readFileSync(__dirname + '/data/jogosPorJogador.json'))
 };
 
 app.set('view engine', 'hbs');
 
-var fs = require('fs');
-db["jogadores"] = fs.readFileSync(__dirname + '/data/jogadores.json');
-
 app.set('views', 'server/views');
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', db.jogadores);
 });
-
-
 
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
@@ -50,3 +50,28 @@ app.get('/', function (req, res) {
 
 // abrir servidor na porta 3000
 // dica: 1-3 linhas de código
+
+app.get('/jogador/:id/', function(req, res) {
+  let perfil = _.find(db.jogadores.players, function(el) { 
+  	     return el.steamid === req.params.id;
+  	  });
+  let jogos = db.jogosPorJogador[req.params.id];
+
+  jogos.not_played_count = _.where(jogos.games, { playtime_forever: 0 }).length;
+  jogos.games = _.sortBy(jogos.games, function(el) {
+    return -el.playtime_forever;
+  });
+  jogos.games = _.head(jogos.games, 5);
+  jogos.games = _.map(jogos.games, function(el) {
+    el.playtime_forever_h = Math.round(el.playtime_forever/60);
+    return el;
+  });
+
+  res.render('jogador', {
+    profile: perfil,
+    gameInfo: jogos,
+    favorito: jogos.games[0]
+  });
+});
+
+
